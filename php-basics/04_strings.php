fcd4938573228506fdd437d7678e872295445cf7
Pranav Maddi <pranavrammaddi@gmail.com>
2026-01-07 16:08:13 +0530

    Understand PHP strings, quotes, and concatenation

---
+++ php-basics/04_strings.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Dexter";
+
+// Using Double Quotes ("")
+
+echo "Using Double Quotes: \n";
+echo "Hello from $name\n\n";
+
+// Using Single Quotes('')
+
+echo 'Using Single Quotes: \n';
+echo ' Hello from $name';
+
+// Using concatenation(.)
+
+echo "\n\nUsing Conacatenation: \n";
+echo "Hello from ".$name."\n";
