f2ae68e64e847c1447574df8899e64279198fbeb
Pranav Maddi <pranavrammaddi@gmail.com>
2026-01-07 15:41:58 +0530

    Understand PHP data types and var_dump output

---
+++ php-basics/02_datatypes.php
@@ -0,0 +1,13 @@
+<?php
+
+$name  = "John Doe";
+$age = 20;
+$isDev = true;
+$height = 5.9;
+$skills = ["PHP", "Laravel", "Git"];
+
+var_dump($name);
+var_dump($age);
+var_dump($isDev);
+var_dump($height);
+var_dump($skills);
