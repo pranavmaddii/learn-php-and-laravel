0cb46988217764417bbe8ba4eaebca64ccf65a81
Pranav Maddi <pranavrammaddi@gmail.com>
2026-01-07 15:58:41 +0530

    Understand echo, print, and var_dump output differences

---
+++ php-basics/03_output_methods.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Jane Doe";
+$age = 21;
+
+// echo
+echo "Using echo: \n";
+echo $name. "<br>";
+
+// print
+print "Using print: \n";
+print $age. "\n";
+
+// var_dump
+echo "Using var_dump(): \n";
+var_dump($name);
+
+// HTML vs CLI difference
+echo "\nHTML tag test:\n";
+echo "Age: $age<br>";
